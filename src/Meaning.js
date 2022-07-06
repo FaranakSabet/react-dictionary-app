ddcc6f6685487f5e63b4cff347f10912437e6226
Faranak Sabet <65681350+FaranakSabet@users.noreply.github.com>
2022-07-06 10:53:34 -0700

    Added Phonetics

--- src/Meaning.js
+++ src/Meaning.js
@@ -9,12 +9,13 @@ export default function Meaning(props){
             return (
               <div key={index}>
                 <p>
-                  <strong>Definition:</strong>{definition.definition}
+                  <strong>Definition:</strong>
+                  {definition.definition}
                   <br />
                   <strong>Example:</strong>
                   <em>{definition.example}</em>
-                  <Synonyms synonyms={definition.synonyms} />
                 </p>
+                <Synonyms synonyms={definition.synonyms} />
               </div>
             );
         })}
