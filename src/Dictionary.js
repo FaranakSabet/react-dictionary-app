06d7a332caa5972806bd29ae344e12726fa43694
Faranak Sabet <65681350+FaranakSabet@users.noreply.github.com>
2022-07-06 15:36:51 -0700

    Added new components

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,34 +1,52 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Results from "./Results";
 import axios from "axios";
-import "./Dictionary.css"
+import "./Dictionary.css";
 
-export default function Dictionary(){
-    let [keyword, setKeyword] = useState("");
-    let [results, setResults] = useState(null);
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
+  let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
-    function handleResponse(response){
-        setResults(response.data[0]);
-       
-    }
+  function handleResponse(response) {
+    setResults(response.data[0]);
 
-    function search(event){
-        event.preventDefault();
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
-        
-    }
+  }
 
-    function handleKeywordChange(event){
-        setKeyword(event.target.value);
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-    }
-    return(
-        <div className="Dictionary">
-            <form onSubmit={search}>
-                <input type="search" onChange={handleKeywordChange}/>
-            </form>
-            <Results results = {results}/>
-        </div>
-    ); 
+  function search() {
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+
+function load(){
+  setLoaded(true);
+  search();
+}
+
+
+  if(loaded){
+    return (
+    <div className="Dictionary">
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input type="search" onChange={handleKeywordChange} />
+        </form>
+      </section>
+      <Results results={results} />
+    </div>
+  );
+  } else {
+    load();
+    return "Loading";
+  }
+  
 }
