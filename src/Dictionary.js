0470005a548df708863a05a9ca83dcee9749575e
Faranak Sabet <65681350+FaranakSabet@users.noreply.github.com>
2022-07-07 10:44:14 -0700

    added pexels api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,9 +8,12 @@ export default function Dictionary(props) {
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
-
+  }
+  
+  function handlePexelsResponse(response){
+    console.log(response)
   }
 
   function handleSubmit(event) {
@@ -20,20 +23,26 @@ export default function Dictionary(props) {
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+    let pexelsApiKey =
+      "563492ad6f917000010000010e63706df4b84a1196af53bcb95b3bb6";
+      let pexelsApiUrl =
+        "https://api.pexels.com/v1/search?query=${keyword}&per_page=1";
+        let headers = {Autorization: `Bearer ${pexelsApiKey}`};
+        axios.get(pexelsApiUrl, {headers: headers}).then(handlePexelsResponse);
+
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
 
-function load(){
-  setLoaded(true);
-  search();
-}
+  function load() {
+    setLoaded(true);
+    search();
+  }
 
-
-  if(loaded){
+  if (loaded) {
     return (
       <div className="Dictionary">
         <section>
@@ -53,5 +62,4 @@ function load(){
     load();
     return "Loading";
   }
-  
 }
