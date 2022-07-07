d17f7327b82c9f3b7a84ff8ff41c105a3a053aea
Faranak Sabet <65681350+FaranakSabet@users.noreply.github.com>
2022-07-06 18:47:08 -0700

    fixed issue

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,7 +6,7 @@ export default function Meaning(props) {
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definition.map(function (definition, index) {
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <div className="definition">{definition.definition}</div>
